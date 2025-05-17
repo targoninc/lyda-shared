90753c0e8e5d33fefec6dd92b28fcf6c16b8a155
alexa <35202909+loudar@users.noreply.github.com>
2025-05-17 18:28:33 +0200

    remove entitytype from interactionconfig

--- src/Models/InteractionConfig.ts
+++ src/Models/InteractionConfig.ts
@@ -1,9 +1,7 @@
 import {InteractionType} from "../Enums/InteractionType.ts";
-import {EntityType} from "../Enums/EntityType.ts";
 
 export interface InteractionConfig {
     type: InteractionType;
-    entityType: EntityType;
     toggleable?: boolean;
     icons: {
         default: string;
