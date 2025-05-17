ab16c8583374baa5931bc04e83f12eb8154c2023
alexa <35202909+loudar@users.noreply.github.com>
2025-05-17 18:21:22 +0200

    add track to collaborator

--- src/Models/db/lyda/TrackCollaborator.ts
+++ src/Models/db/lyda/TrackCollaborator.ts
@@ -1,8 +1,10 @@
 import {User} from "./User.js";
 import {CollaboratorType} from "./CollaboratorType.js";
+import {Track} from "./Track.ts";
 
 export interface TrackCollaborator {
     collab_type?: CollaboratorType;
+    track?: Track;
     user?: User;
     track_id: number;
     user_id: number;
