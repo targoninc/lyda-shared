739b516c465544135cbb6664d58427246e165f0c
alexa <35202909+loudar@users.noreply.github.com>
2025-05-18 20:09:34 +0200

    add two enums

---
+++ src/Enums/CollabTypes.ts
@@ -0,0 +1,9 @@
+export enum CollabTypes {
+    collaborator = 'Collaborator',
+    producer = 'Producer',
+    composer = 'Composer',
+    singer = 'Singer',
+    original = 'Original',
+    remixer = 'Remixer',
+    visualArtist = 'Visual artist',
+}
