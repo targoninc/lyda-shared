0b3ccd1af55227c3242306b994725d7c8fbf0760
alexa <35202909+loudar@users.noreply.github.com>
2025-05-30 11:20:53 +0200

    add optional entity to playingfrom

--- src/Models/PlayingFrom.ts
+++ src/Models/PlayingFrom.ts
@@ -1,5 +1,9 @@
+import {Album} from "./db/lyda/Album.ts";
+import {Playlist} from "./db/lyda/Playlist.ts";
+
 export interface PlayingFrom {
     type: "album" | "playlist" | string;
     name: string;
     id: number;
+    entity?: Album|Playlist;
 }
