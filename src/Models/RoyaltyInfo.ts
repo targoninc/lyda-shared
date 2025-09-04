34c8ce37b0d13e6a9ce8b9331869167cd727e218
alexa <alex@targoninc.com>
2025-09-04 22:55:09 +0200

    update royalty info model

--- src/Models/RoyaltyInfo.ts
+++ src/Models/RoyaltyInfo.ts
@@ -1,11 +1,16 @@
-import {RoyaltyMonth} from "./RoyaltyMonth.ts";
-
 export interface RoyaltyInfo {
-    calculatableMonths: RoyaltyMonth[];
-    trackRoyaltyValues: any;
-    meanTrackRoyalty: number;
-    paidTotal: number;
-    available: number,
-    totalRoyalties: number,
-    paypalMail: string | null,
+    personal: {
+        trackRoyaltyValues: any;
+        meanTrackRoyalty: number;
+        paidTotal: number;
+        available: number,
+        totalRoyalties: number,
+        paypalMail: string | null,
+    },
+    global: {
+        meanTrackRoyalty: number;
+        paidTotal: number,
+        totalRoyalties: number,
+        trackRoyaltyValues: any;
+    }
 }
