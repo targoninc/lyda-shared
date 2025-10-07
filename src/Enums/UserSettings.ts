0cfd4087298001b53d7e11435fac9df59d30f9f9
alexa <alex@targoninc.com>
2025-10-07 11:09:17 +0200

    make user setting an enum

--- src/Enums/UserSettings.ts
+++ src/Enums/UserSettings.ts
@@ -1,13 +1,13 @@
-export const UserSettings = {
-    notificationCollaboration: "notification_collaboration",
-    notificationComment: "notification_comment",
-    notificationFollow: "notification_follow",
-    notificationLike: "notification_like",
-    notificationRepost: "notification_repost",
-    paypalMail: "paypalMail",
-    publicLikes: "publicLikes",
-    theme: "theme",
-    streamingQuality: "streamingQuality",
-    playFromAutoQueue: "playFromAutoQueue",
-    language: "language",
-};
+export enum UserSettings {
+    notificationCollaboration = "notification_collaboration",
+    notificationComment = "notification_comment",
+    notificationFollow = "notification_follow",
+    notificationLike = "notification_like",
+    notificationRepost = "notification_repost",
+    paypalMail = "paypalMail",
+    publicLikes = "publicLikes",
+    theme = "theme",
+    streamingQuality = "streamingQuality",
+    playFromAutoQueue = "playFromAutoQueue",
+    language = "language",
+}
