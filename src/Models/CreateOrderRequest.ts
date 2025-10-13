b3b20cd152bad14de9196a687c3a62c0bc91c264
alexa <alex@targoninc.com>
2025-10-13 22:59:54 +0200

    fix model

--- src/Models/CreateOrderRequest.ts
+++ src/Models/CreateOrderRequest.ts
@@ -4,7 +4,7 @@ import {PaymentProvider} from "../Enums/PaymentProvider.ts";
 export const CreateOrderRequest = z.object({
     type: z.enum(["album", "track"]),
     paymentProvider: z.enum(PaymentProvider),
-    entityId: z.uint64(),
+    entityId: z.number(),
     priceInUsd: z.number().min(0.01).max(100).optional(),
 });
 
