8b6fe67a2c489dfa1ad28946edcc1f4c5edd3e3c
alexa <alex@targoninc.com>
2025-10-13 21:59:00 +0200

    add order model

---
+++ src/Enums/OrderStatus.ts
@@ -0,0 +1,5 @@
+export enum OrderStatus {
+    Started = "started",
+    Fulfilled = "fulfilled",
+    Cancelled = "cancelled",
+}
---
+++ src/Models/db/finance/Order.ts
@@ -0,0 +1,11 @@
+export interface Order {
+    id: string;
+    payment_provider: string;
+    item_type: string;
+    provider_request_id: string;
+    provider_order_id: string;
+    provider_status: string;
+    status: string;
+    user_id: string;
+    item_id: string;
+}
