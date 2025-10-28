c1bbcddb6138ccd7df9dd05c6b7a24381ca7139d
alexa <alex@targoninc.com>
2025-10-28 15:55:33 +0100

    fix order model

--- src/Models/db/finance/Order.ts
+++ src/Models/db/finance/Order.ts
@@ -2,10 +2,10 @@ export interface Order {
     id: string;
     payment_provider: string;
     item_type: string;
-    provider_request_id: string;
     provider_order_id: string;
     provider_status: string;
     status: string;
+    amount_ct: number;
     user_id: string;
     item_id: number;
     created_at: Date;
