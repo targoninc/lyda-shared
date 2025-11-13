a46d62bdd2f2800c2c09112adf2bb4fb92752d81
alex <alex@targoninc.com>
2025-11-13 13:39:36 +0100

    add shuffling

--- src/Enums/UserCacheKey.ts
+++ src/Enums/UserCacheKey.ts
@@ -1,6 +1,7 @@
 export enum UserCacheKey {
     volume = "volume",
     loopMode = "loopMode",
+    shuffling = "shuffling",
     lastTrackId = "lastTrackId",
     lastTrackPosition = "lastTrackPosition",
     playingFrom = "playingFrom",
