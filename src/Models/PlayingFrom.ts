acff371ac5abeac1e2bc706b5978fe6e40cccc21
alex <alex@targoninc.com>
2025-12-08 20:29:06 +0100

    add feedtype to playingfrom

--- src/Models/PlayingFrom.ts
+++ src/Models/PlayingFrom.ts
@@ -1,5 +1,6 @@
 import {Album} from "./db/lyda/Album.ts";
 import {Playlist} from "./db/lyda/Playlist.ts";
+import {FeedType} from "../Enums/FeedType.ts";
 
 /**
  * Represents the source from where media is currently being played.
@@ -9,4 +10,5 @@ export interface PlayingFrom {
     name: string;
     id?: number;
     entity?: Album | Playlist;
+    feedType?: FeedType;
 }
