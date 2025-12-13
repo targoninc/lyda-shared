5ce3dc0d64697b9ba88794ce7d0e773cdda7d34d
alex <alex@targoninc.com>
2025-12-13 16:38:01 +0100

    differentiate streaming + sale earnings

--- src/Models/RoyaltyMonth.ts
+++ src/Models/RoyaltyMonth.ts
@@ -2,7 +2,8 @@ export interface RoyaltyMonth {
     month: number;
     year: number;
     hasEarnings: boolean;
-    earnings: number;
+    streamingEarnings: number;
+    saleEarnings: number;
     artistRoyalties: number;
     trackRoyalties: number;
     calculated: boolean;
