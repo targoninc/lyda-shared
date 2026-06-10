5687d43f132e057491d9b76dcef417ed65cd5eff
alex <alex@targoninc.com>
2026-06-11 00:18:34 +0200

    new columns on payout

--- src/Models/db/finance/Payout.ts
+++ src/Models/db/finance/Payout.ts
@@ -5,6 +5,10 @@ export interface Payout {
     payout_batch_id: string;
     user_id: number;
     amount_ct: number;
+    currency_code: string;
+    stripe_transfer_id: string | null;
+    stripe_payout_id: string | null;
+    reversed_amount_ct: number;
     status: PaymentStatus;
     created_at: Date;
     updated_at: Date;
