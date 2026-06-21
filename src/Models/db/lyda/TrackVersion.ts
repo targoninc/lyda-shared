0ee4a2c49f17557114d4ec7301bd9c2d8b517dc0
alex <alex@targoninc.com>
2026-06-21 21:11:30 +0200

    track versions v1

--- src/Models/db/lyda/TrackVersion.ts
+++ src/Models/db/lyda/TrackVersion.ts
@@ -4,5 +4,8 @@ export interface TrackVersion extends Entity {
     track_id: number;
     index: number;
     name: string | null;
+    loudness_data: string;
+    processed: boolean;
+    length: number;
     created_at: Date;
 }
